2fccd1cf43d769bd7b234dee5d90cafc153d7a8a
Andrey Dunaev <dunayev@yandex.ru>
2023-05-08 10:10:35 +0700

    fix исправить алгоритм работы кнопок, исправить название шрифта, текс в ридми

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,21 @@ const buttonTwo = document.querySelector(".button-two");
 const text = document.querySelector(".text-with-buttons__text-container");
 buttonOne.addEventListener("click", () => {
   buttonOne.classList.toggle("text-with-buttons__button_active");
-  buttonOne.dataset.state === "on" ? (buttonOne.dataset.state = "off") : (buttonOne.dataset.state = "on");
+  setDataset(buttonOne);
+  buttonTwo.addEventListener("click", {});
 });
 buttonTwo.addEventListener("click", () => {
   buttonTwo.classList.toggle("text-with-buttons__button_active");
-  buttonTwo.dataset.state === "on" ? (buttonTwo.dataset.state = "off") : (buttonTwo.dataset.state = "on");
-  if (buttonOne.dataset.state === buttonTwo.dataset.state) {
-    text.classList.toggle("text-with-buttons__text-contsiner_hide");
+  setDataset(buttonTwo);
+
+  if (buttonOne.dataset.state === "on" && buttonTwo.dataset.state === "on") {
+    text.classList.toggle("text-with-buttons__text-container_hide");
     buttonOne.classList.remove("text-with-buttons__button_active");
     buttonTwo.classList.remove("text-with-buttons__button_active");
-    buttonOne.dataset.state = "off";
-    buttonTwo.dataset.state = "off";
   }
+  buttonOne.dataset.state = "off";
+  buttonTwo.dataset.state = "off";
 });
+function setDataset(item) {
+  return item.dataset.state === "on" ? (item.dataset.state = "off") : (item.dataset.state = "on");
+}
